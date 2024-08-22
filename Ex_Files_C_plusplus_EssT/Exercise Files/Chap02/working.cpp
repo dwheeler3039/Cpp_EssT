15cae4a0bc188d7a57f8169c0dfc7e04834df838
dwheeler3039 <52417272+dwheeler3039@users.noreply.github.com>
2024-08-22 22:42:02 +0000

    removed the formatting call

--- Ex_Files_C_plusplus_EssT/Exercise Files/Chap02/working.cpp
+++ Ex_Files_C_plusplus_EssT/Exercise Files/Chap02/working.cpp
@@ -1,11 +1,12 @@
 // working.cpp by Bill Weinman [bw.org]
 // updated 2022-05-21
-#include <format>
+// #include <format>
 #include <iostream>
 
-using std::format;
-using std::cout;
+// using std::format;
+using std::cout, std::endl;
 
 int main() {
     cout << "Hello, World!\n";
+    cout << endl << endl;
 }
